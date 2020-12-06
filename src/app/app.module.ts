7123403d42b81db7f8501b5cb749002f5905b583
siddharth <siddharth.ashutosh7@gmail.com>
2020-12-06 14:55:08 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { warningAlertComponent } from './warningAlert/warningAlert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
     SuccessAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,10 +8,38 @@ import { Component } from '@angular/core';
 export class ServerComponent {
   serverId: number = 10
   serverStatus: string = 'online'
+  serverText: string = ' ' 
+  userText: string = ''
+  buttonText: boolean = true
+  toggle:boolean = false
+  click: number = 0
+  arrayNum = []
   constructor() { //this is called when class is created
     this.serverId = 20;
   }
   onClicking() {
     this.serverId = 100;
+  }
+  onTypingName(event){
+    this.serverText = event.target.value
+  }
+  checkButton(){
+    if(this.userText.length > 0){
+      return this.buttonText = true
+    } else {
+      return this.buttonText = false
+    }
+  }
+  onReset(){
+    this.userText = ''
+  }
+  onToggle(){
+    if(this.toggle == false){
+      this.toggle = true
+    }else{
+      this.toggle = false
+    }
+    this.click = this.click + 1
+    this.arrayNum.push(new Date())
   }
 }
